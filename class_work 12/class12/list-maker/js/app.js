ee1341e1d9ab3f2a8d9eeabc065a09a9c2ce73a1
carloscesitar <carloscesitar@hotmail.com>
2017-01-28 15:15:05 -0500

    Homework correction
    Focus on #item after the item is added. I didn't get that right.

--- class_work 12/class12/list-maker/js/app.js
+++ class_work 12/class12/list-maker/js/app.js
@@ -3,26 +3,33 @@
 // 2. Click on #clickme
 
 
-$('#clickme').click(function(){
+
+$(document).ready(function () {
+
+	$('#clickme').click(function(){
 	
-	var newItem = '<li>' + $('#item').val() + '</li>';
+		
+		var newItem = '<li>' + $('#item').val() + '</li>';
 
 	// $('#list').append(newItem);
 
 	// Itermediate Bonus: If the value of #item is blank, do not append the <li> and alert user (hint: use an if/else statement)
 
-	if (newItem == '<li></li>') {
-	alert('Please enter an item');
-	}
+		if (newItem == '<li></li>') {
+		alert('Please enter an item');
+		}
 
-	else {
+		else {
 
-	appendItem (newItem);
+		appendItem (newItem);
 
-	$('#item').val('');
+		$('#item').val('');
 
-	}
-})
+		$('#item').focus().css('outline-width', 'medium').css('outline-style', 'solid');
+		
+		
+		}
+	})
 
 
 // 3. New item is appended as a <li> element to the #list
@@ -32,40 +39,41 @@ $('#clickme').click(function(){
 // called when #clickme is clicked and appends the <li> to #list
 
 
-function appendItem (item) {
+	function appendItem (item) {
 	
 	$('#list').append(item);
-}
+	}
 
 
 
 
 // Bonus: Focus on #item after the item is added (hint: look up "jquery focus")
 
+	
+	// $('#item').focus(function(){
+	// 		$(this).css('outline-width', 'medium').css('outline-style', 'solid');
+	// 	})
 
-$('#item').focus(function(){
-	$(this).css('outline-width', 'medium');
-})
 
-
-$('#item').blur(function(){
-	// $(this).removeAttr('style');
-	$(this).css({'outline-width':''});
-})
+	// $('#item').blur(function(){
+	// 	$(this).removeAttr('style');
+	// 	// $(this).css({'outline-width':''});
+	// })
 
 
 
 // Legendary Bonus: Remove individual <li> elements when they are clicked (hint: use $(this) and .remove())
 
 
-$('ul').on('click', 'li', function(){
-	$(this).remove();
-})
+	$('ul').on('click', 'li', function(){
+		$(this).remove();
+		$('#item').blur().removeAttr('style');
+	})
 
 
 
 
-// $(document).ready(function () {
+
 
 	// 1. Attach a .click() event to #clickme; attach an anonymous function to the .click()
 	// 1A. This anonymous function should create a variable, newItem equal to the value of #item
@@ -73,4 +81,4 @@ $('ul').on('click', 'li', function(){
 	// 1C. This anonymous function should then clear the text inside of #item
 
 	// 2. Write the appendItem function here
-// })
+})

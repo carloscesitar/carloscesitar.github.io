81cb44c5ab68aa9b9091d966a540893483b4d20a
carloscesitar <carloscesitar@hotmail.com>
2017-02-16 21:32:42 -0500

    HOMEWORK!!!!!!!!!!!!!!!!!!!!
    Raindrops (radiohead song as background music)

---
+++ class_work 17/class17/raindrops-example/js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+// $('.box').raindrops(
+// {color:'#f77b7b',
+// canvasHeight:150, 
+// waveLength: 100,
+// rippleSpeed: 0.05, 
+// density: 0.04});
+
+$('.box').raindrops(
+{color:'cadetblue',
+density: 0.1,
+frequency: 20,
+rippleSpeed: 0.05});
+
+
+})
